4b5a841fac3ce4cc37ba68b76cd0081d13cdf101
James Tadd Adcox <jamestaddadcox@gmail.com>
2023-08-08 15:08:26 -0400

    ugly lil modal component but it works

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -19,7 +19,8 @@ if(currentToken != null) {
 export default new Vuex.Store({
   state: {
     token: currentToken || '',
-    user: currentUser || {}
+    user: currentUser || {},
+    modalFlag: false,
   },
   mutations: {
     SET_AUTH_TOKEN(state, token) {
@@ -37,6 +38,9 @@ export default new Vuex.Store({
       state.token = '';
       state.user = {};
       axios.defaults.headers.common = {};
-    }
+    },
+    //CHANGE_MODAL_FLAG(state, modalFlag) {
+    //  this.modalFlag = !this.modalFlag;
+    //}
   }
 })
